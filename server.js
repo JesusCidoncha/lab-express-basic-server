ead2f52280d69ab656787a99c43657144c4875df
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2024-01-16 12:51:12 +0100

    completed lab

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const projects = require("./data/projects.json");
+const articles = require("./data/articles.json");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/home.html");
+});
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/blog.html");
+});
+app.get("/api/projects", (req, res) => {
+  res.json(projects);
+});
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
+});
+
+app.listen(3000, () => console.log("Listening on server 3000!"));
